6a6a899a6fb5272e75968fc4cb1979673eaeaf26
Kunal Shankar <90144667+kunalbhole@users.noreply.github.com>
2026-08-29 12:46:27 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
 }
 
@@ -34,7 +35,7 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
+        composeOptions {
         kotlinCompilerExtensionVersion = "1.5.14"
     }
 
